5030ba96248a3309064934b03afeada4bab58f57
escapedcat <github@htmlcss.de>
2021-04-27 11:31:54 +0800

    feat(cli): add basic function and test
    add an extra line and another word

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rusty-cli
+
 dabbling around to create a simple CLI with rust
+
+# Resources used
+
+- https://www.youtube.com/watch?v=zF34dRivLOw
+- https://www.youtube.com/watch?v=IhvzGzoq4mc
---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::env;
+
+fn get_tx_info(tx: &str) -> String {
+    format!("{} append", &tx.to_string())
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let command = args[1].clone();
+   
+    if command == "tx-info" {
+        let tx = args[2].clone();
+        println!("TX info: {}", get_tx_info(&tx.to_string()));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_get_tx_info() {
+        assert_eq!(get_tx_info("12"), "12 append");
+    }
+}
