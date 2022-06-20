572ac6a45b9b8fb0ed44a3082fbbc913c5eecca6
escapedcat <github@htmlcss.de>
2022-06-20 05:59:55 +0000

    Deploying to gh-pages from @ escapedcat/rusty-cli@f0b3f0b5e6ba5586d5953a7cdb8c48b33d345742 🚀

--- search-index.js
+++ search-index.js
@@ -1,4 +1,4 @@
 var searchIndex = JSON.parse('{\
-"rusty_cli":{"doc":"","t":[5,5],"n":["get_tx_info","main"],"q":["rusty_cli",""],"d":["Get a tx info from a tx",""],"i":[0,0],"f":[[[["str",15]],["string",3]],[[]]],"p":[]}\
+"rusty_cli":{"doc":"","t":[5,5],"n":["get_tx_info","main"],"q":["rusty_cli",""],"d":["Get a tx info from a tx",""],"i":[0,0],"f":[[[["str",0]],["string",3]],[[]]],"p":[]}\
 }');
-initSearch(searchIndex);
+if (window.initSearch) {window.initSearch(searchIndex)};
